c41520bdf136778dbec018c16e299fd2d16c6ca6
Florian Winkler <winkler@b1-systems.de>
2020-08-05 21:54:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+helm repo add harbor https://helm.goharbor.io/
+helm repo update
+kubectl create namespace harbor-poc
 helm install harbor harbor/harbor --namespace harbor-poc --values harbor/values.yaml
